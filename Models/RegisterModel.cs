c27a6ccc9e03de2a6edb3cf331d38c2c961c7935
jtucfknight <56446602+jtucfknight@users.noreply.github.com>
2019-10-13 15:02:25 -0400

    Add files via upload

---
+++ Models/RegisterModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodTrace.Models
+{
+    class RegisterModel
+    {
+        //This model class holds the register information
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
